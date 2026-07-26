583e694175b824b75e43c2eb94b002eadffcae92
24f1001299 <24f1001299@ds.study.iitm.ac.in>
2026-07-26 18:26:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,17 +5,14 @@ app = FastAPI()
 
 @app.post("/prorate")
 async def prorate(req: Request):
-    body = await req.json()
+    b = await req.json()
 
-    old_price = body["old_price"]
-    new_price = body["new_price"]
-    year = body["year"]
-    month = body["month"]
-    upgrade_day = body["upgrade_day"]
+    old, new = b["old_price"], b["new_price"]
+    year, month, day = b["year"], b["month"], b["upgrade_day"]
 
-    days_in_month = calendar.monthrange(year, month)[1]
-    days_remaining = days_in_month - upgrade_day + 1
+    dim = calendar.monthrange(year, month)[1]
+    remaining = dim - day + 1
 
-    charge = round((new_price - old_price) * (days_remaining / days_in_month), 2)
+    charge = round((new - old) * (remaining / dim), 2)
 
     return {"charge": charge}
